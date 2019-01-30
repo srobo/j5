1f2544c3487085543c803ce67b616209917074f0
Dan Trickey <dan@trickey.io>
2019-01-30 19:20:31 +0000

    Update Version Number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="j5",
-    version="0.0.1",
+    version="0.0.2",
     author="Dan Trickey",
     author_email="contact@trickey.io",
     description="J5 Robotics API",
