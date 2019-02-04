528cfcab8d0fe49a6c357c2f08ccf00e7be09f77
HU90m <hugo.mcnally@gmail.com>
2019-02-04 19:35:57 +0000

    fixed buzzer component style

--- j5/components/buzzer.py
+++ j5/components/buzzer.py
@@ -7,19 +7,18 @@ from j5.components import Component
 
 
 class BuzzerInterface(metaclass=ABCMeta):
-    """ An interface containing the methods required to control an buzzer.
-    """
+    """An interface containing the methods required to control an buzzer."""
+
     @abstractmethod
-    def buzz(self, board:Board, identifier:int, length:int,
-                   frequency:int = None, note:str = None   ) -> None:
-        """ Set the state of an buzzer.
-        """
+    def buzz(self, board: Board, identifier: int, length: int,
+             frequency: int = None, note: str = None) -> None:
+        """Set the state of an buzzer."""
         raise NotImplementedError  # pragma: no cover
 
 
 class Buzzer(Component):
-    """ A standard buzzer.
-    """
+    """A standard buzzer."""
+
     def __init__(self, identifier: int, board: Board, backend: BuzzerInterface) -> None:
         self._board = board
         self._backend = backend
@@ -27,6 +26,5 @@ class Buzzer(Component):
 
     @staticmethod
     def interface_class():
-        """ Get the interface class that is required to use this component.
-        """
+        """Get the interface class that is required to use this component."""
         return BuzzerInterface
