3afb5170305bc37288677c3567879578d7cab337
HU90m <hugo.mcnally@gmail.com>
2019-02-04 19:06:16 +0000

    add buzzer components

---
+++ j5/components/buzzer.py
@@ -0,0 +1,32 @@
+"""Classes for the Buzzer support."""
+
+from abc import ABCMeta, abstractmethod
+
+from j5.boards import Board
+from j5.components import Component
+
+
+class BuzzerInterface(metaclass=ABCMeta):
+    """ An interface containing the methods required to control an buzzer.
+    """
+    @abstractmethod
+    def buzz(self, board:Board, identifier:int, length:int,
+                   frequency:int = None, note:str = None   ) -> None:
+        """ Set the state of an buzzer.
+        """
+        raise NotImplementedError  # pragma: no cover
+
+
+class Buzzer(Component):
+    """ A standard buzzer.
+    """
+    def __init__(self, identifier: int, board: Board, backend: BuzzerInterface) -> None:
+        self._board = board
+        self._backend = backend
+        self._identifier = identifier
+
+    @staticmethod
+    def interface_class():
+        """ Get the interface class that is required to use this component.
+        """
+        return BuzzerInterface
---
+++ tests/components/test_buzzer.py
@@ -0,0 +1,54 @@
+"""Tests for the Buzzer Classes."""
+from j5.backends import Backend
+from j5.boards import Board
+from j5.components.buzzer import Buzzer, BuzzerInterface
+
+
+class TestingBuzzerDriver(BuzzerInterface):
+    """ A testing driver for the buzzer.
+    """
+    def buzz(self, board:Board, identifier:int, length:int,
+                   frequency:int = None, note:str = None   ) -> None:
+        """ Set the state of an buzzer.
+        """
+        pass
+
+
+class TestingBuzzerBoard(Board):
+    """ A testing board for the buzzer.
+    """
+    @property
+    def name(self) -> str:
+        """ The name of this board.
+        """
+        return "Testing Buzzer Board"
+
+    @property
+    def serial(self) -> str:
+        """ The serial number of this board.
+        """
+        return "SERIAL"
+
+    @property
+    def components(self):
+        """ List the components that this Board supports.
+        """
+        return [Buzzer]
+
+    @staticmethod
+    def discover(backend: Backend):
+        """ Detect all of the boards on a given backend.
+        """
+        return []
+
+
+def test_buzzer_interface_implementation():
+    """ Test that we can implement the BuzzerInterface.
+    """
+    TestingBuzzerDriver()
+
+
+def test_buzzer_instantiation():
+    """ Test that we can instantiate an buzzer.
+    """
+    Buzzer(0, TestingBuzzerBoard(), TestingBuzzerDriver())
