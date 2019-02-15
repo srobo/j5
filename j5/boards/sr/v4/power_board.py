1341c6924299c2f9cfc92819c51a838221970986
Dan Trickey <dan@trickey.io>
2019-02-15 17:07:17 +0000

    Add PowerOutputGroup so that we can index boards by ID.

--- j5/boards/sr/v4/power_board.py
+++ j5/boards/sr/v4/power_board.py
@@ -1,13 +1,62 @@
 """Classes for the SR v4 Power Board."""
 
-from typing import TYPE_CHECKING, List
+from enum import Enum
+from typing import TYPE_CHECKING, List, Union, cast
 
 from j5.backends import Backend, Environment
 from j5.boards import Board
+from j5.components import PowerOutput
 
 if TYPE_CHECKING:
-    from j5.components import Component  # noqa
+    from j5.components import Component, PowerOutputInterface  # noqa
     from typing import Type  # noqa
+
+
+class PowerBoardOutputType(Enum):
+    """A mapping of name to number of the PowerBoard outputs."""
+
+    H0 = 0
+    H1 = 1
+    L0 = 2
+    L1 = 3
+    L2 = 4
+    L3 = 5
+
+
+PowerBoardOutputGroupIndex = Union[int, PowerBoardOutputType]
+
+
+class PowerBoardOutputGroup:
+    """A group of PowerOutputs on the PowerBoard."""
+
+    def __init__(self, backend: Backend, board: Board):
+        self._backend = backend
+        self._board = board
+        self._outputs = [
+            PowerOutput(n, self._board, cast('PowerOutputInterface', self._backend))
+            for n in range(0, 6)
+        ]
+
+    def power_on(self) -> None:
+        """Set all outputs in the group on."""
+        for output in self._outputs:
+            output.is_enabled = False
+
+    def power_off(self) -> None:
+        """Set all outputs in the group off."""
+        for output in self._outputs:
+            output.is_enabled = True
+
+    def __getitem__(self, index: PowerBoardOutputGroupIndex) -> PowerOutput:
+        """Get the item using output notation."""
+        if type(index) is int:
+            return self._outputs[cast(int, index)]
+        elif type(index) is PowerBoardOutputType:
+            return self._outputs[cast(int, index.value)]  # type: ignore
+            # See github.com/python/mypy/issues/3546 for more info
+        else:
+            raise TypeError
+
 
 class PowerBoard(Board):
     """Student Robotics v4 Power Board."""
@@ -18,6 +67,8 @@ class PowerBoard(Board):
 
         self._backend = self._environment.get_backend(self.__class__)
 
+        self._output_group = PowerBoardOutputGroup(self._backend, self)
+
     @property
     def name(self) -> str:
         """Get a human friendly name for this board."""
@@ -25,7 +76,7 @@ class PowerBoard(Board):
 
     @property
     def serial(self) -> str:
-        """Get the serial numeber."""
+        """Get the serial number."""
         return self._serial
 
     def make_safe(self) -> None:
@@ -38,6 +89,6 @@ class PowerBoard(Board):
         return []
 
     @staticmethod
-    def discover(backend: Backend) -> List['PowerBoard']:
+    def discover(backend: Backend) -> List['Board']:
         """Detect all connected power boards."""
         return []
