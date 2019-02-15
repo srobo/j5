067489a36404057ce3fe7b18b44c09accdbb8238
Kier Davis <kierdavis@gmail.com>
2019-02-15 18:15:06 +0000

    shell.nix: decouple location of nixpkgs source from its configuration

--- shell.nix
+++ shell.nix
@@ -1,9 +1,12 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  pkgsSrc ? <nixpkgs>,
+  pkgs ? import pkgsSrc {}
+}:
 
 # Note that as of 2019-01-21, pipenv is currently broken in the NixOS 18.09 release.
 # https://github.com/NixOS/nixpkgs/issues/51970
 # To work around, invoke with:
-# nix-shell -E 'import ./shell.nix { pkgs = import (fetchTarball https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz) {}; }'
+# nix-shell -E 'import ./shell.nix { pkgsSrc = fetchTarball https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz; }'
 
 with pkgs;
 
