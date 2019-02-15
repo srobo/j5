f995998be6ff508c7cb7789fd69892cdb43d9da7
Kier Davis <kierdavis@gmail.com>
2019-02-15 17:13:17 +0000

    Add Nix environment definition

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+# Note that as of 2019-01-21, pipenv is currently broken in the NixOS 18.09 release.
+# https://github.com/NixOS/nixpkgs/issues/51970
+# To work around, invoke with:
+# nix-shell -E 'import ./shell.nix { pkgs = import (fetchTarball https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz) {}; }'
+
+with pkgs;
+
+stdenv.mkDerivation {
+  name = "j5-dev-env";
+  buildInputs = [
+    pipenv
+    python3
+  ];
+}
