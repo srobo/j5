0e6bf381c99404ff6e1cf598a5bd4da2c6490826
Dan Trickey <dan@trickey.io>
2019-02-19 22:46:35 +0000

    Split BackendMeta.__new__ into more understandable functions.

--- j5/backends/base.py
+++ j5/backends/base.py
@@ -25,20 +25,30 @@ class BackendMeta(ABCMeta):
         if hasattr(cls, "environment"):
             if cls.environment is not None and cls.board is not None:
 
-                if type(cls.environment) != Environment:
-                    raise ValueError("The environment must be of type Environment.")
-
-                if cls.board in cls.environment.supported_boards:
-                    raise RuntimeError("You cannot register multiple backends for the same board in the same Environment.")  # noqa: E501
-
-                for component in cls.board.supported_components():
-                    if not issubclass(cls, component.interface_class()):
-                        raise TypeError("The backend class doesn't have a required interface.")  # noqa: E501
+                mcs._check_compatibility(cls)
+                mcs._check_component_interfaces(cls)
 
                 cls.environment.register_backend(cls.board, cls)
                 return cls
 
         raise RuntimeError(f"The {str(cls)} has no environment attribute")
+
+    def _check_compatibility(cls):  # type: ignore
+        """Check that the backend and environment are compatible."""
+
+        if type(cls.environment) != Environment:
+            raise ValueError("The environment must be of type Environment.")
+
+        if cls.board in cls.environment.supported_boards:
+            raise RuntimeError(
+                "You cannot register multiple backends for the same board in the same Environment.")  # noqa: E501
+
+    def _check_component_interfaces(cls):  # type: ignore
+        """Check that the backend has the interfaces required to support the board components."""
+
+        for component in cls.board.supported_components():
+            if not issubclass(cls, component.interface_class()):
+                raise TypeError("The backend class doesn't have a required interface.")  # noqa: E501
 
 
 class Backend(metaclass=BackendMeta):
