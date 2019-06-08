5d283bb6702e6b6b46e1051425efe6d0fafc3091
Dan Trickey <dan@trickey.io>
2019-06-08 11:16:20 +0100

    Add Derived Ultrasound Component.

---
+++ j5/components/derived/ultrasound.py
@@ -0,0 +1,83 @@
+"""
+Ultrasonic distance sensor.
+
+A sensor that utilises the reflection of ultrasound to calculate the distance
+to a nearby object.
+"""
+
+from abc import abstractmethod
+from datetime import timedelta
+from typing import Optional, Type
+
+from j5.components.component import DerivedComponent, Interface
+from j5.components.gpio_pin import GPIOPin
+
+
+class UltrasoundInterface(Interface):
+    """An interface containing the methods required for an UltrasoundSensor."""
+
+    @abstractmethod
+    def get_ultrasound_pulse(self, pin_tx: int, pin_rx: int) -> Optional[timedelta]:
+        """
+        Get a timedelta for the ultrasound time.
+
+        Returns None if the sensor times out.
+        """
+        raise NotImplementedError  # pragma: no cover
+
+    @abstractmethod
+    def get_ultrasound_distance(self, pin_tx: int, pin_rx: int) -> Optional[float]:
+        """Get a distance in metres."""
+        raise NotImplementedError  # pragma: no cover
+
+
+class UltrasoundSensor(DerivedComponent):
+    """
+    Ultrasonic distance sensor.
+
+    A sensor that utilises the reflection of ultrasound to calculate the distance
+    to a nearby object.
+    """
+
+    def __init__(
+        self,
+        gpio_tx: GPIOPin,
+        gpio_rx: GPIOPin,
+        backend: UltrasoundInterface,
+        *,
+        distance_mode: bool = True,
+    ) -> None:
+        self._gpio_tx = gpio_tx
+        self._gpio_rx = gpio_rx
+        self._backend = backend
+        self._distance_mode = distance_mode
+
+    @staticmethod
+    def interface_class() -> Type[Interface]:
+        """Get the interface class that is required to use this component."""
+        return UltrasoundInterface
+
+    def pulse(self) -> Optional[timedelta]:
+        """
+        Send a pulse and return the time taken.
+
+        Returns None if timeout occurred.
+        """
+        return self._backend.get_ultrasound_pulse(
+            self._gpio_tx.identifier,
+            self._gpio_rx.identifier,
+        )
+
+    def distance(self) -> Optional[float]:
+        """
+        Send a pulse and return the distance to the object.
+
+        Returns none if a timeout occurred.
+        """
+        if not self._distance_mode:
+            raise Exception("Distance mode is disabled. Use pulse() to get the time.")
+
+        return self._backend.get_ultrasound_distance(
+            self._gpio_tx.identifier,
+            self._gpio_rx.identifier,
+        )
