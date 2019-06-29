2f97265ec876b984b79cccca47cd8a7cd24a39b4
Hugo McNally <hugo.mcnally@gmail.com>
2019-06-29 09:22:58 +0100

    first draft of markers

--- j5/vision/markers.py
+++ j5/vision/markers.py
@@ -1,75 +1,212 @@
 """Markers Class."""
 
-from typing import NamedTuple
+from __future__ import annotations
+
+from typing import List, NamedTuple, Union
 
 import numpy as np
 
 
-class CylCoords(NamedTuple):
-    """Cylindrical Coordinate System.
+class Cartesian(NamedTuple):
+    """A Cartesian Coordinate."""
+
+    x: float
+    y: float
+    z: float
+
+    def as_cartesian(self) -> Cartesian:
+        """Provides the coordiante in the form of a cartesian coordinate."""
+        return self
+
+    def as_cylindrical(self) -> Cylindrical:
+        """Provides the coordiante in the form of a cylindrical coordinate."""
+        return Cylindrical(np.sqrt(self.x**2 + self.y**2),
+                           np.arctan2(self.y, self.x),
+                           self.z)
+
+    def as_spherical(self) -> Spherical:
+        """Provides the coordiante in the form of a spherical coordinate."""
+        return Spherical(np.sqrt(self.x**2 + self.y**2 + self.z**2),
+                         np.arctan2(self.y, self.x),
+                         np.arctan2(np.sqrt(self.x**2 + self.y**2), self.z))
+
+
+class Cylindrical(NamedTuple):
+    """A Cylindrical Coordinate.
 
     p   := axial distance
     phi := azimuth angle (radians)
     z   := height
     """
 
-    p: np.array
-    phi: np.array
-    z: np.array
+    p: float
+    phi: float
+    z: float
+
+    def as_cartesian(self) -> Cartesian:
+        """Provides the coordiante in the form of a cartesian coordinate."""
+        return Cartesian(self.p * np.cos(self.phi),
+                         self.p * np.sin(self.phi),
+                         self.z)
+
+    def as_cylindrical(self) -> Cylindrical:
+        """Provides the coordiante in the form of a cylindrical coordinate."""
+        return self
+
+    def as_spherical(self) -> Spherical:
+        """Provides the coordiante in the form of a spherical coordinate."""
+        return Spherical(np.sqrt(self.p**2 + self.z**2),
+                         np.arctan2(self.p, self.z),
+                         self.phi)
 
 
-class SphCoords(NamedTuple):
-    """Spherical Coordinate System.
+class Spherical(NamedTuple):
+    """A Spherical Coordinate.
 
     r     := radial distance
     theta := polar angle (radians)
     phi   := azimuth angle (radians)
     """
 
-    r: np.array
-    theta: np.array
-    phi: np.array
+    r: float
+    theta: float
+    phi: float
+
+    def as_cartesian(self) -> Cartesian:
+        """Provides the coordiante in the form of a cartesian coordinate."""
+        return Cartesian(self.r * np.cos(self.phi) * np.sin(self.theta),
+                         self.r * np.sin(self.phi) * np.sin(self.theta),
+                         self.r * np.cos(self.theta))
+
+    def as_cylindrical(self) -> Cylindrical:
+        """Provides the coordiante in the form of a cylindrical coordinate."""
+        return Cylindrical(self.r * np.sin(self.theta),
+                           self.phi,
+                           self.r * np.cos(self.theta))
+
+    def as_spherical(self) -> Spherical:
+        """Provides the coordiante in the form of a spherical coordinate."""
+        return self
 
 
-class CartCoords(NamedTuple):
-    """Cartesian Coordinate System."""
+Coordinate = Union[Cylindrical, Spherical, Cartesian]
 
-    x: np.array
-    y: np.array
-    z: np.array
+
+class Orientation(NamedTuple):
+    """The orientation of the tuple represented as a quarternion."""
+
+    x: float
+    y: float
+    z: float
+    w: float
 
 
 class Markers():
-    """Object Class."""
+    """Markers Class.
+
+    The position is stored in self.__position.
+    This is a multi-dementional numpy array.
+    The first dimension is the axis.
+    the second dimesion is the marker index.
+    Example:
+        [ [x1, y1, z1],
+          [x2, y2, z2],
+          [x3, y3, z3],
+          [x4, y4, z4] ]
+    """
+
+    def __init__(self):
+        self.__positions = None
+
+    @property
+    def positions(self) -> List[Cartesian]:
+        """Returns the makers positions."""
+        return [Cartesian(*coordinate) for coordinate in self.__positions]
+
+    def add_markers_cartesian(self, given_positions: List[Cartesian]) -> None:
+        """Add marker(s) by providing their cartesian coordinates."""
+        given_positions = np.array(given_positions)
+        self.__append_positions(given_positions)
+
+    def add_markers(self, given_positions: List[Coordinate]) -> None:
+        """Add marker(s) by providing their coordinates."""
+        given_positions = np.array([position.to_cartesian()
+                                    for position in given_positions
+                                    ])
+        self.__append_positions(given_positions)
+
+    def __append_positions(self, given_positions):
+        """Appends a numpy array of markers to the current array (if one exists)."""
+        if self.__positions is not None:
+            self.__positions = np.concatenate([self.__positions, given_positions])
+        else:
+            self.__positions = given_positions
+
+    def translate_space(self, delta_x, delta_y, delta_z):
+        """Move translate the markers."""
+        self.__positions \
+            = self.__positions \
+            + np.array([delta_x, delta_y, delta_z])
+
+    def rotate_about_x(self, rot_x):
+        """Rotate all the markers about the x axis."""
+        x_rotation = np.array([[1, 0, 0],
+                               [0, np.cos(rot_x), -np.sin(rot_x)],
+                               [0, np.sin(rot_x), np.cos(rot_x)]])
+        self.__positions = np.multiply(self.__positions, x_rotation)
+
+    def rotate_about_y(self, rot_y):
+        """Rotate all the markers about the y axis."""
+        y_rotation = np.array([[np.cos(rot_y), 0, np.sin(rot_y)],
+                               [0, 1, 0],
+                               [-np.sin(rot_y), 0, np.cos(rot_y)]])
+        self.__positions = np.multiply(self.__positions, y_rotation)
+
+    def rotate_about_z(self, rot_z):
+        """Rotate all the markers about the z axis."""
+        z_rotation = np.array([[np.cos(rot_z), -np.sin(rot_z), 0],
+                               [np.sin(rot_z), np.cos(rot_z), 0],
+                               [0, 0, 1]])
+        self.__positions = np.multiply(self.__positions, z_rotation)
 
     @staticmethod
-    def cart_to_cyl_coords(cart: CartCoords) -> CylCoords:
-        """Converts Cartesian Coordinates to Cylindrical Coordinates."""
-        cyl = CylCoords(np.sqrt(cart.x**2 + cart.y**2),
-                        np.arctan2(cart.y, cart.x),
-                        cart.z)
-        return cyl
+    def cartesian_to_cylindrical(cart: Cartesian) -> Cylindrical:
+        """Converts a cartesian coordinate to a cylindrical coordinate."""
+        return Cylindrical(np.sqrt(cart.x**2 + cart.y**2),
+                           np.arctan2(cart.y, cart.x),
+                           cart.z)
 
     @staticmethod
-    def sph_to_cyl_coords(sph: SphCoords) -> CylCoords:
-        """Converts Spherical Coordinates to Cylindrical Coordinates."""
-        cyl = CylCoords(sph.r * np.sin(sph.theta),
-                        sph.phi,
-                        sph.r * np.cos(sph.theta))
-        return cyl
+    def cartesian_to_spherical(cart: Cartesian) -> Spherical:
+        """Converts a cartesian coordinate to a spherical coordinate."""
+        return Spherical(np.sqrt(cart.x**2 + cart.y**2 + cart.z**2),
+                         np.arctan2(cart.y, cart.x),
+                         np.arctan2(np.sqrt(cart.x**2 + cart.y**2), cart.z))
 
     @staticmethod
-    def cyl_to_cart_coords(cly: CylCoords) -> CartCoords:
-        """Converts Cylindrical Coordinates to Cartesian Coordinates."""
-        cart = CartCoords(cly.p * np.cos(cly.phi),
-                          cly.p * np.sin(cly.phi),
-                          cly.z)
-        return cart
+    def cylindrical_to_cartesian(cyl: Cylindrical) -> Cartesian:
+        """Converts a cylindrical coordinate to a cartesian coordinate."""
+        return Cartesian(cyl.p * np.cos(cyl.phi),
+                         cyl.p * np.sin(cyl.phi),
+                         cyl.z)
 
     @staticmethod
-    def cyl_to_sph_coords(cyl: CylCoords) -> SphCoords:
-        """Converts Cylindrical Coordinates to Spherical Coordinates."""
-        sph = SphCoords(np.sqrt(cyl.p**2 + cyl.z**2),
-                        np.arctan2(cyl.p, cyl.z),
-                        cyl.phi)
-        return sph
+    def cylindrical_to_spherical(cyl: Cylindrical) -> Spherical:
+        """Converts a cylindrical coordinate to a spherical coordinate."""
+        return Spherical(np.sqrt(cyl.p**2 + cyl.z**2),
+                         np.arctan2(cyl.p, cyl.z),
+                         cyl.phi)
+
+    @staticmethod
+    def spherical_to_cylindrical(sph: Spherical) -> Cylindrical:
+        """Converts a spherical coordinate to a cylindrical coordinate."""
+        return Cylindrical(sph.r * np.sin(sph.theta),
+                           sph.phi,
+                           sph.r * np.cos(sph.theta))
+
+    @staticmethod
+    def spherical_to_cartesian(sph: Spherical) -> Cartesian:
+        """Converts a spherical coordinate to a cartesian coordinate."""
+        return Cartesian(sph.r * np.cos(sph.phi) * np.sin(sph.theta),
+                         sph.r * np.sin(sph.phi) * np.sin(sph.theta),
+                         sph.r * np.cos(sph.theta))
--- tests/vision/test_markers.py
+++ tests/vision/test_markers.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-from j5.vision.marker import CartCoords, CylCoords, Markers, SphCoords
+from j5.vision.markers import Cartesian, Cylindrical, Markers, Spherical
 
 
 def test_markers_instantiation() -> None:
@@ -10,66 +10,66 @@ def test_markers_instantiation() -> None:
     Markers()
 
 
-def test_markers_cart_to_cyl_coords() -> None:
-    """Test the cart_to_cyl_coords() method of markers class."""
-    cartesian = CartCoords(np.array([3, 20]),
-                           np.array([4, 3]),
-                           np.array([5, 7]))
+def test_markers_cartesian_to_cylindrical() -> None:
+    """Test the cartesian_to_cylindrical() method of markers class."""
+    cartesian_1 = Cartesian(3., 4., 5.)
+    cartesian_2 = Cartesian(20., 3., 7.)
 
-    expected_output = CylCoords(np.array([5, 20.223748416157]),
-                                np.array([0.92729521800161,
-                                          0.1488899476095]),
-                                np.array([5, 7]))
-    actual_output = Markers.cart_to_cyl_coords(cartesian)
+    expected_output_1 = Cylindrical(5., 0.92729521800161, 5.)
+    expected_output_2 = Cylindrical(20.223748416157, 0.1488899476095, 7)
 
-    assert np.allclose(expected_output, actual_output)
+    actual_output_1 = Markers.cartesian_to_cylindrical(cartesian_1)
+    actual_output_2 = Markers.cartesian_to_cylindrical(cartesian_2)
 
-
-def test_markers_sph_to_cyl_coords() -> None:
-    """Test the sph_to_cyl_coords() method of markers class."""
-    spherical = SphCoords(np.array([5, 10]),
-                          np.array([0.25 * np.pi, 0.60 * np.pi]),
-                          np.array([0.50 * np.pi, 0.01 * np.pi]))
-
-    expected_output = CylCoords(np.array([3.535533906,
-                                          9.510565163]),
-                                np.array([1.570796327,
-                                          0.03141592654]),
-                                np.array([3.535533906,
-                                          -3.090169944]))
-    actual_output = Markers.sph_to_cyl_coords(spherical)
-
-    assert np.allclose(expected_output, actual_output)
+    assert np.allclose(expected_output_1, actual_output_1)
+    assert np.allclose(expected_output_2, actual_output_2)
 
 
-def test_markers_cyl_to_cart_coords() -> None:
-    """Test the cyl_to_cart_coords() method of markers class."""
-    cylindrical = CylCoords(np.array([3, 10]),
-                            np.array([0.43 * np.pi, 1.62 * np.pi]),
-                            np.array([2, 4]))
+def test_markers_spherical_to_cylindrical() -> None:
+    """Test the spherical_to_cylindrical() method of markers class."""
+    spherical_1 = Spherical(5, 0.25 * np.pi, 0.50 * np.pi)
+    spherical_2 = Spherical(10, 0.60 * np.pi, 0.01 * np.pi)
 
-    expected_output = CartCoords(np.array([0.6544297242,
-                                           3.681245527]),
-                                 np.array([2.927750286,
-                                           -9.297764859]),
-                                 np.array([2, 4]))
-    actual_output = Markers.cyl_to_cart_coords(cylindrical)
+    expected_output_1 = Cylindrical(3.535533906,
+                                    1.570796327,
+                                    3.535533906)
 
-    assert np.allclose(expected_output, actual_output)
+    expected_output_2 = Cylindrical(9.510565163,
+                                    0.03141592654,
+                                    -3.090169944)
+
+    actual_output_1 = Markers.spherical_to_cylindrical(spherical_1)
+    actual_output_2 = Markers.spherical_to_cylindrical(spherical_2)
+
+    assert np.allclose(expected_output_1, actual_output_1)
+    assert np.allclose(expected_output_2, actual_output_2)
 
 
-def test_markers_cyl_to_sph_coords() -> None:
-    """Test the cyl_to_sph_coords() method of markers class."""
-    cylindrical = CylCoords(np.array([3, 10]),
-                            np.array([0.43 * np.pi, 1.62 * np.pi]),
-                            np.array([2, 4]))
+def test_markers_cylindrical_to_cartesian() -> None:
+    """Test the cylindrical_to_cartesian() method of markers class."""
+    cylindrical_1 = Cylindrical(3., 0.43 * np.pi, 2.)
+    cylindrical_2 = Cylindrical(10., 1.62 * np.pi, 4.)
 
-    expected_output = SphCoords(np.array([3.605551275,
-                                          10.77032961]),
-                                np.array([0.9827937233,
-                                          1.19028995]),
-                                np.array([1.350884841,
-                                          5.089380099]))
-    actual_output = Markers.cyl_to_sph_coords(cylindrical)
+    expected_output_1 = Cartesian(0.6544297242, 2.927750286, 2.)
+    expected_output_2 = Cartesian(3.681245527, -9.297764859, 4.)
 
-    assert np.allclose(expected_output, actual_output)
+    actual_output_1 = Markers.cylindrical_to_cartesian(cylindrical_1)
+    actual_output_2 = Markers.cylindrical_to_cartesian(cylindrical_2)
+
+    assert np.allclose(expected_output_1, actual_output_1)
+    assert np.allclose(expected_output_2, actual_output_2)
+
+
+def test_markers_cylindrical_to_spherical() -> None:
+    """Test the cylindrical_to_spherical() method of markers class."""
+    cylindrical_1 = Cylindrical(3., 0.43 * np.pi, 2.)
+    cylindrical_2 = Cylindrical(10., 1.62 * np.pi, 4.)
+
+    expected_output_1 = Spherical(3.605551275, 0.9827937233, 1.350884841)
+    expected_output_2 = Spherical(10.77032961, 1.19028995, 5.089380099)
+
+    actual_output_1 = Markers.cylindrical_to_spherical(cylindrical_1)
+    actual_output_2 = Markers.cylindrical_to_spherical(cylindrical_2)
+
+    assert np.allclose(expected_output_1, actual_output_1)
+    assert np.allclose(expected_output_2, actual_output_2)
