aa51837d730841ff3615018e14d20c4b7cc8b7a7
Dan Trickey <dan@trickey.io>
2019-07-16 11:52:43 +0100

    Add tests for MarkerList

--- tests/vision/test_markers.py
+++ tests/vision/test_markers.py
@@ -1,7 +1,23 @@
 """Test classes that represent Markers."""
 
+from random import randint, random
+
 from j5.vision.coordinates import Coordinate
-from j5.vision.markers import Marker
+from j5.vision.markers import Marker, MarkerList
+
+
+def get_random_float() -> float:
+    """Get a random float between -8 and 8."""
+    return (random() - 0.5) * randint(0, 2 * 8)
+
+
+def get_random_marker() -> Marker:
+    """Get a random marker for testing."""
+    marker_id = randint(0, 128)
+    return Marker(
+        marker_id,
+        Coordinate(get_random_float(), get_random_float(), get_random_float()),
+    )
 
 
 def test_markers_instantiates() -> None:
@@ -21,3 +37,15 @@ def test_markers_positions() -> None:
 
     assert(given_id == marker.id)
     assert(given_position == marker.position)
+
+
+def test_marker_list_instantiation() -> None:
+    """Test that we can instantiate a MarkerList."""
+    li = [get_random_marker() for _ in range(0, randint(0, 12))]
+
+    marker_list = MarkerList(li)
+    assert isinstance(marker_list, MarkerList)
+    iter(marker_list)  # Test it's iterable
+
+    marker = marker_list[0]
+    assert isinstance(marker, Marker)
