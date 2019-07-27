e9cd2145bad590717c71747bec733300007a5d78
Dan Trickey <dan@trickey.io>
2019-07-28 00:48:05 +0100

    Release 0.7.0

--- j5/__init__.py
+++ j5/__init__.py
@@ -5,5 +5,5 @@ from .boards import BoardGroup
 
 __all__ = ["BoardGroup", "BaseRobot", "__version__", "__version_short__"]
 
-__version__ = "0.5.0"
-__version_short__ = "0.5.0"
+__version__ = "0.7.0"
+__version_short__ = "0.7.0"
