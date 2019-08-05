6fb2ab82b51ebab18c52f6b390a652aa46f9ca9f
Kier Davis <kierdavis@gmail.com>
2019-08-05 21:50:16 +0100

    Bump version for 0.7.3 release

--- j5/__init__.py
+++ j5/__init__.py
@@ -5,5 +5,5 @@ from .boards import BoardGroup
 
 __all__ = ["BoardGroup", "BaseRobot", "__version__", "__version_short__"]
 
-__version__ = "0.7.2"
-__version_short__ = "0.7.2"
+__version__ = "0.7.3"
+__version_short__ = "0.7.3"
