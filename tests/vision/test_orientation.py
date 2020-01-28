adabe1ab430c421b51c1b24bee68bcf74f4c8b15
Tommy Poll <Tommy@poolle.co.uk>
2020-01-28 20:15:34 +0000

    Correct docstrings & add full stops

--- tests/vision/test_orientation.py
+++ tests/vision/test_orientation.py
@@ -12,13 +12,13 @@ def test_quaternion() -> None:
 
 
 def test_matrix() -> None:
-    """Test the matrix property of the Orientation class"""
+    """Test the matrix property of the Orientation class."""
     q = Quaternion.random()
     assert q.rotation_matrix == Orientation(q).matrix
 
 
 def test_yaw_pitch_roll() -> None:
-    """Test the yaw_pitch_roll property of the Orientation class"""
+    """Test the yaw_pitch_roll property of the Orientation class."""
     q = Quaternion.random()
     assert q.yaw_pitch_roll == Orientation(q).yaw_pitch_roll
 
@@ -30,37 +30,37 @@ def test_yaw() -> None:
 
 
 def test_pitch() -> None:
-    """Test the yaw property of the Orientation class."""
+    """Test the pitch property of the Orientation class."""
     q = Quaternion.random()
     assert q.yaw_pitch_roll[1] == Orientation(q).pitch
 
 
 def test_roll() -> None:
-    """Test the yaw property of the Orientation class."""
+    """Test the roll property of the Orientation class."""
     q = Quaternion.random()
     assert q.yaw_pitch_roll[2] == Orientation(q).roll
 
 
 def test_rot_x() -> None:
-    """Test the yaw property of the Orientation class."""
+    """Test the rot_x property of the Orientation class."""
     q = Quaternion.random()
     assert q.yaw_pitch_roll[1] == Orientation(q).rot_x
 
 
 def test_rot_y() -> None:
-    """Test the yaw property of the Orientation class."""
+    """Test the rot_y property of the Orientation class."""
     q = Quaternion.random()
     assert q.yaw_pitch_roll[0] == Orientation(q).rot_y
 
 
 def test_rot_z() -> None:
-    """Test the yaw property of the Orientation class."""
+    """Test the rot_z property of the Orientation class."""
     q = Quaternion.random()
     assert q.yaw_pitch_roll[2] == Orientation(q).rot_z
 
 
 def test_repr() -> None:
-    """Test the yaw property of the Orientation class."""
+    """Test the __repr__ method of the Orientation class."""
     o = Orientation(Quaternion.random())
     assert repr(o) == f"Orientation(" \
                       f"x_radians={o.rot_x}, " \
