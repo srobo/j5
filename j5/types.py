c69579956504b4872c25ebc5784b5a39735f8f5b
Dan Trickey <dan@trickey.io>
2020-05-31 14:55:46 +0100

    Add ImmutableDict and ImmutableList types.

--- j5/types.py
+++ j5/types.py
@@ -1,5 +1,5 @@
 """Useful datatypes that aren't available in the standard lib."""
-from typing import Generic, Iterator, Mapping, TypeVar
+from typing import Generator, Generic, Iterator, List, Mapping, TypeVar, Union
 
 T = TypeVar("T")
 U = TypeVar("U")
@@ -18,6 +18,25 @@ class ImmutableDict(Generic[T, U]):
     def __iter__(self) -> Iterator[U]:
         """Iterate over the members of the group."""
         return iter(self._members.values())
+
+    def __len__(self) -> int:
+        """Get the number of members in the group."""
+        return len(self._members)
+
+
+class ImmutableList(Generic[T]):
+    """A list whose items cannot be set."""
+
+    def __init__(self, members: Union[List[T], Generator[T, None, None]]):
+        self._members: List[T] = list(members)
+
+    def __getitem__(self, index: int) -> T:
+        """Get an member using list notation."""
+        return self._members[index]
+
+    def __iter__(self) -> Iterator[T]:
+        """Iterate over the members of the group."""
+        return iter(self._members)
 
     def __len__(self) -> int:
         """Get the number of members in the group."""
