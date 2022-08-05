c1b729895cdfdcc6a9ac8875fda2b4a97eadb2b0
Dan Trickey <git@trickey.io>
2022-08-05 20:48:25 +0100

    stubs: Some data from pyserial is Optional
    This is a partial causes of #783

--- stubs/serial/tools/list_ports_common.pyi
+++ stubs/serial/tools/list_ports_common.pyi
@@ -1,14 +1,16 @@
 """Type stubs for serial.tools.list_ports_common."""
+from typing import Optional
 
 class ListPortInfo:
     """Info collection base class for serial ports"""
 
     device: str
-    vid: int
-    pid: int
-    serial_number: str
-    manufacturer: str
-    product: str
+    vid: Optional[int]
+    pid: Optional[int]
+    serial_number: Optional[str]
+    manufacturer: Optional[str]
+    product: Optional[str]
 
 
     def __init__(self, device: str, skip_link_detection: bool = False) -> None: ...
+    def usb_info(self) -> str: ...
