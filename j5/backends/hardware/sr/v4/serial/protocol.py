b90a0f7f15345bee4392ca4613693c53f0129b20
Dan Trickey <git@trickey.io>
2022-09-11 13:32:38 +0100

    backends/sr-serial: Reorder methods in class

--- j5/backends/hardware/sr/v4/serial/protocol.py
+++ j5/backends/hardware/sr/v4/serial/protocol.py
@@ -36,6 +36,16 @@ class SRV4SerialProtocolBackend(SerialHardwareBackend, ABC):
 
     _lock: threading.Lock
 
+    @property
+    def firmware_version(self) -> str:
+        """
+        The firmware version reported by the board.
+
+        :returns: firmware version reported by the board, if any.
+        """
+        identity = self.get_identity()
+        return identity.software_version
+
     def get_identity(self) -> BoardIdentity:
         """
         Get the board identity information.
@@ -58,62 +68,6 @@ class SRV4SerialProtocolBackend(SerialHardwareBackend, ABC):
         This turns off all the board's outputs, LEDs, etc.
         """
         self.request("*RESET")
-
-    @property
-    def firmware_version(self) -> str:
-        """
-        The firmware version reported by the board.
-
-        :returns: firmware version reported by the board, if any.
-        """
-        identity = self.get_identity()
-        return identity.software_version
-
-    @staticmethod
-    def _parse_version_string(version: str) -> BoardVersion:
-        """
-        Parse the version string.
-
-        If the minor version is not present, it is assumed to be 0.
-
-        :param version: The version string.
-        :returns: A parsed version string.
-        :raises ValueError: The version string was not valid.
-        """
-        match = re.match(r"^(\d+)\.(\d+)(?:\.(\d+))?$", version)
-        if not match:
-            raise ValueError(f"Version does not match format: {version}")
-
-        version_parts = (int(x) for x in match.groups(0))
-        return BoardVersion(*version_parts)
-
-    def _check_firmware_version_supported(
-        self,
-        version_string: str,
-        minimum_fw_version: int = 4,
-    ) -> None:
-        """
-        Raises an exception if the firmware version is not supported.
-
-        :param version_string: the firmware version string to check.
-        :param minimum_fw_version: the lowest supported major firmware version
-        :raises CommunicationError: the board is running unsupported firmware
-        """
-        try:
-            version = self._parse_version_string(version_string)
-        except ValueError as e:
-            raise CommunicationError(f"Unable to parse version number: {version}") from e
-
-        if version.hw_ver != 4:
-            raise CommunicationError(
-                f"Expected hardware version number to be 4, got {version.hw_ver}",
-            )
-
-        if version.fw_major < minimum_fw_version:
-            raise CommunicationError(
-                f"Expected major version number of at least {minimum_fw_version},"
-                f" got {version.fw_major}",
-            )
 
     def request(self, command: str) -> None:
         """
@@ -182,3 +136,49 @@ class SRV4SerialProtocolBackend(SerialHardwareBackend, ABC):
             raise CommunicationError(f"Power Board returned an error: {error_string}")
 
         return response
+
+    @staticmethod
+    def _parse_version_string(version: str) -> BoardVersion:
+        """
+        Parse the version string.
+
+        If the minor version is not present, it is assumed to be 0.
+
+        :param version: The version string.
+        :returns: A parsed version string.
+        :raises ValueError: The version string was not valid.
+        """
+        match = re.match(r"^(\d+)\.(\d+)(?:\.(\d+))?$", version)
+        if not match:
+            raise ValueError(f"Version does not match format: {version}")
+
+        version_parts = (int(x) for x in match.groups(0))
+        return BoardVersion(*version_parts)
+
+    def _check_firmware_version_supported(
+        self,
+        version_string: str,
+        minimum_fw_version: int = 4,
+    ) -> None:
+        """
+        Raises an exception if the firmware version is not supported.
+
+        :param version_string: the firmware version string to check.
+        :param minimum_fw_version: the lowest supported major firmware version
+        :raises CommunicationError: the board is running unsupported firmware
+        """
+        try:
+            version = self._parse_version_string(version_string)
+        except ValueError as e:
+            raise CommunicationError(f"Unable to parse version number: {version}") from e
+
+        if version.hw_ver != 4:
+            raise CommunicationError(
+                f"Expected hardware version number to be 4, got {version.hw_ver}",
+            )
+
+        if version.fw_major < minimum_fw_version:
+            raise CommunicationError(
+                f"Expected major version number of at least {minimum_fw_version},"
+                f" got {version.fw_major}",
+            )
